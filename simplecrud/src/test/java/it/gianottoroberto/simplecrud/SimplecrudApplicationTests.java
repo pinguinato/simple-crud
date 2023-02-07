c97c12016a6309cbe60f63f7d32d615d9a7916f8
Roberto Gianotto <rgianotto-ext@skylogic.it>
2023-02-07 15:32:22 +0100

    fix unit test

--- simplecrud/src/test/java/it/gianottoroberto/simplecrud/SimplecrudApplicationTests.java
+++ simplecrud/src/test/java/it/gianottoroberto/simplecrud/SimplecrudApplicationTests.java
@@ -3,7 +3,7 @@ package it.gianottoroberto.simplecrud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SimplecrudApplicationTests.class)
 class SimplecrudApplicationTests {
 
 	@Test
