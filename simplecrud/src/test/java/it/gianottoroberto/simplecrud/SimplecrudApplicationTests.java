82c4158540c50499c19f5391325dee1b2568485c
Roberto Gianotto <rgianotto-ext@skylogic.it>
2023-02-07 09:29:42 +0100

    commit iniziale dell'applicazione

---
+++ simplecrud/src/test/java/it/gianottoroberto/simplecrud/SimplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gianottoroberto.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
